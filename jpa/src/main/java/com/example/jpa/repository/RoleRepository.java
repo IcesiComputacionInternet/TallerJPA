672509e8c7095b3885326d0e0ec097682c88dc8c
Santiago Arevalo Valencia <santiguitar2003@gmail.com>
2023-03-17 14:51:08 -0500

    feat-RoleRepository implemented, with findByName() method added

--- jpa/src/main/java/com/example/jpa/repository/RoleRepository.java
+++ jpa/src/main/java/com/example/jpa/repository/RoleRepository.java
@@ -1,10 +1,15 @@
 package com.example.jpa.repository;
 
 import com.example.jpa.model.IcesiRole;
+import com.example.jpa.model.IcesiUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface RoleRepository {
+@Repository
+public interface RoleRepository extends JpaRepository<IcesiRole, UUID> {
 
     Optional<IcesiRole> findByName(String role);
 }
