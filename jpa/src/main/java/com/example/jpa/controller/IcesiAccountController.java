47da35a23cce24a2e92145ab068cbfe8f13d4429
Santiago Arevalo Valencia <santiguitar2003@gmail.com>
2023-03-18 03:24:07 -0500

    feat-Account controller class created to test in Postman

--- jpa/src/main/java/com/example/jpa/controller/IcesiAccountController.java
+++ jpa/src/main/java/com/example/jpa/controller/IcesiAccountController.java
@@ -1,4 +1,27 @@
 package com.example.jpa.controller;
 
+import com.example.jpa.dto.AccountRequestDTO;
+import com.example.jpa.dto.AccountResponseDTO;
+import com.example.jpa.service.AccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/accounts")
 public class IcesiAccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping
+    public AccountResponseDTO createAccount(@RequestBody AccountRequestDTO accountRequestDTO) {
+        return accountService.save(accountRequestDTO);
+    }
+
+    @GetMapping
+    public List<AccountResponseDTO> getAccount() {
+        return accountService.getAccounts();
+    }
 }
