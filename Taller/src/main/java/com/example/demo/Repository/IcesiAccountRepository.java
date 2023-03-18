2e128d76d504c83e7da42e419b16bb94e6a8bf3e
BrayanOB2003 <brayanortizbastidas82@gmail.com>
2023-03-18 00:40:23 -0500

    Added repositories

---
+++ Taller/src/main/java/com/example/demo/Repository/IcesiAccountRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.Repository;
+
+import com.example.demo.model.IcesiAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IcesiAccountRepository extends JpaRepository<IcesiAccount, UUID> {
+
+}
---
+++ Taller/src/main/java/com/example/demo/Repository/IcesiRoleRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.Repository;
+
+import com.example.demo.model.IcesiRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IcesiRoleRepository extends JpaRepository<IcesiRole, UUID> {
+
+}
