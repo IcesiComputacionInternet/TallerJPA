82c7397e516f395037ebf9cf0ecbc836a7095b40
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2023-03-18 23:35:46 -0500

    Account service implemented

--- demo/src/main/java/co/edu/icesi/demo/repository/IcesiAccountRepository.java
+++ demo/src/main/java/co/edu/icesi/demo/repository/IcesiAccountRepository.java
@@ -1,11 +1,19 @@
 package co.edu.icesi.demo.repository;
 
 import co.edu.icesi.demo.model.IcesiAccount;
+import co.edu.icesi.demo.model.IcesiRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface IcesiAccountRepository extends JpaRepository<IcesiAccount, UUID> {
+
+    @Query("SELECT a FROM IcesiAccount a WHERE a.accountNumber = :accountNumber")
+    Optional<IcesiRole> findByAccountNumber(String accountNumber);
+
+
 }
--- demo/src/main/java/co/edu/icesi/demo/service/IcesiAccountService.java
+++ demo/src/main/java/co/edu/icesi/demo/service/IcesiAccountService.java
@@ -1,10 +1,14 @@
 package co.edu.icesi.demo.service;
 
+import co.edu.icesi.demo.dto.IcesiAccountDto;
 import co.edu.icesi.demo.mapper.IcesiAccountMapper;
+import co.edu.icesi.demo.model.IcesiAccount;
 import co.edu.icesi.demo.repository.IcesiAccountRepository;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -12,4 +16,26 @@ import org.springframework.stereotype.Service;
 public class IcesiAccountService {
     private final IcesiAccountRepository icesiAccountRepository;
     private final IcesiAccountMapper icesiAccountMapper;
+
+    public IcesiAccount saveAccount(IcesiAccountDto accountToSave){
+        if(icesiAccountRepository.findByAccountNumber(accountToSave.getAccountNumber()).isPresent()){
+            throw new RuntimeException("Account number taken");
+        }
+        if(accountToSave.getBalance()<0){
+            throw new RuntimeException("Balance can´t be below 0");
+        }
+        IcesiAccount icesiAccount = icesiAccountMapper.fromIcesiAccountDto(accountToSave);
+        icesiAccount.setAccountId(UUID.randomUUID());
+        //Extremely questionable way of generating the account number tbh
+        String accountNumber = "";
+        for (int i =0;i<13;i++){
+            if(i==3 || i==10){
+                accountNumber+="-";
+            }else{
+                accountNumber+= ""+Math.floor(Math.random()*9);
+            }
+        }
+        icesiAccount.setAccountNumber(accountNumber);
+        return icesiAccountRepository.save(icesiAccount);
+    }
 }
