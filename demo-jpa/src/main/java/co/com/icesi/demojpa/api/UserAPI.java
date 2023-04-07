8eb75fbc6b9a883051ec20bd4c6b8c262405ac39
DaniloErazo <erazodanilo1218@gmail.com>
2023-04-07 15:13:01 -0500

    Added API for user

---
+++ demo-jpa/src/main/java/co/com/icesi/demojpa/api/UserAPI.java
@@ -0,0 +1,23 @@
+package co.com.icesi.demojpa.api;
+
+import co.com.icesi.demojpa.dto.RequestUserDTO;
+import co.com.icesi.demojpa.dto.ResponseUserDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+public interface UserAPI {
+
+    String BASE_USER_URL = "/users";
+
+    @GetMapping("{userEmail}")
+    ResponseUserDTO getUser(@PathVariable String userEmail);
+
+    List<ResponseUserDTO> getAllUsers();
+
+    @PostMapping
+    ResponseUserDTO addUser(RequestUserDTO requestUserDTO);
+
+}
