238d04f2b1bbecabade923e6213a73466d9542a3
GabrielSB19 <gasuarez2002@gmail.com>
2023-04-18 10:48:36 -0500

    Finished the implementation of the API

--- TallerJPA/src/main/java/com/icesi/TallerJPA/api/AccountAPI.java
+++ TallerJPA/src/main/java/com/icesi/TallerJPA/api/AccountAPI.java
@@ -9,36 +9,22 @@ public interface AccountAPI {
 
     String ACCOUNT_BASE_URL = "/account";
 
-
     @PostMapping
     IcesiAccountResponseDTO createIcesiAccount(@RequestBody IcesiAccountDTO icesiAccountDTO);
-    /*
 
-    @PatchMapping("/account/activeAccount/{accountNumber}")
-    public String activeAccount(@PathVariable String accountNumber){
-        return accountService.activeAccount(accountNumber);
-    }
+    @PatchMapping("/activeAccount/{accountNumber}")
+    String activeAccount(@PathVariable String accountNumber);
 
-    @PatchMapping("/account/inactiveAccount/{accountNumber}")
-    public String inactiveAccount(@PathVariable String accountNumber){
-        return accountService.disableAccount(accountNumber);
-    }
+    @PatchMapping("/inactiveAccount/{accountNumber}")
+    String inactiveAccount(@PathVariable String accountNumber);
 
-    @PatchMapping("/account/withdrawal/{accountNumber}")
-    public String withdrawalAccount(@PathVariable String accountNumber, @RequestBody Long value){
-        return accountService.withdrawal(accountNumber, value);
-    }
+    @PatchMapping("/withdrawal/{accountNumber}")
+    String withdrawalAccount(@PathVariable String accountNumber, @RequestBody Long value);
 
-    @PatchMapping("/account/deposit/{accountNumber}")
-    public String depositAccount(@PathVariable String accountNumber, @RequestBody Long value){
-        return accountService.deposit(accountNumber, value);
-    }
+    @PatchMapping("/deposit/{accountNumber}")
+    String depositAccount(@PathVariable String accountNumber, @RequestBody Long value);
 
-    @PatchMapping("/account/transfer/{accountNumberOrigin}/{accountNumberDestination}")
-    public String transferAccount(@PathVariable String accountNumberOrigin, @PathVariable String accountNumberDestination, @RequestBody Long value){
-        return accountService.transfer(accountNumberOrigin, accountNumberDestination, value);
-    }
-
-*/
+    @PatchMapping("/transfer/{accountNumberOrigin}/{accountNumberDestination}")
+    String transferAccount(@PathVariable String accountNumberOrigin, @PathVariable String accountNumberDestination, @RequestBody Long value);
 
 }
--- TallerJPA/src/main/java/com/icesi/TallerJPA/controller/IcesiAccountController.java
+++ TallerJPA/src/main/java/com/icesi/TallerJPA/controller/IcesiAccountController.java
@@ -1,5 +1,6 @@
 package com.icesi.TallerJPA.controller;
 
+import com.icesi.TallerJPA.api.AccountAPI;
 import com.icesi.TallerJPA.dto.request.IcesiAccountDTO;
 import com.icesi.TallerJPA.dto.response.IcesiAccountResponseDTO;
 import com.icesi.TallerJPA.service.AccountService;
@@ -8,35 +9,36 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-public class IcesiAccountController {
+public class IcesiAccountController implements AccountAPI {
 
     private final AccountService accountService;
-    @PostMapping("/account")
+
+    @Override
     public IcesiAccountResponseDTO createIcesiAccount(@RequestBody IcesiAccountDTO icesiAccountDTO){
         return  accountService.save(icesiAccountDTO);
     }
 
-    @PatchMapping("/account/activeAccount/{accountNumber}")
+    @Override
     public String activeAccount(@PathVariable String accountNumber){
         return accountService.activeAccount(accountNumber);
     }
 
-    @PatchMapping("/account/inactiveAccount/{accountNumber}")
+    @Override
     public String inactiveAccount(@PathVariable String accountNumber){
         return accountService.disableAccount(accountNumber);
     }
 
-    @PatchMapping("/account/withdrawal/{accountNumber}")
+    @Override
     public String withdrawalAccount(@PathVariable String accountNumber, @RequestBody Long value){
         return accountService.withdrawal(accountNumber, value);
     }
 
-    @PatchMapping("/account/deposit/{accountNumber}")
+    @Override
     public String depositAccount(@PathVariable String accountNumber, @RequestBody Long value){
         return accountService.deposit(accountNumber, value);
     }
 
-    @PatchMapping("/account/transfer/{accountNumberOrigin}/{accountNumberDestination}")
+    @Override
     public String transferAccount(@PathVariable String accountNumberOrigin, @PathVariable String accountNumberDestination, @RequestBody Long value){
         return accountService.transfer(accountNumberOrigin, accountNumberDestination, value);
     }
