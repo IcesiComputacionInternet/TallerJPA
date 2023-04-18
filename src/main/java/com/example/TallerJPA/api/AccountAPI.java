326f9f22397f3cbea78312668cb9062e56746f05
Mateo <mateora73@gmail.com>
2023-04-18 17:38:01 -0500

    Developing APIs

--- src/main/java/com/example/TallerJPA/api/AccountAPI.java
+++ src/main/java/com/example/TallerJPA/api/AccountAPI.java
@@ -1,7 +1,10 @@
 package com.example.TallerJPA.api;
 
+import com.example.TallerJPA.dto.AccountResponseDTO;
+
 public interface AccountAPI {
     String BASE_ACCOUNT_URL = "/accounts";
 
+    public AccountResponseDTO createAccount(AccountResponseDTO accountResponseDTO);
 
 }
