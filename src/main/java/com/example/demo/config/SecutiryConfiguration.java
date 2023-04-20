80446bf90ee9ed52d87ffd2b25a223f487e00d1d
Rockthor1106 <jan.carvajal1@u.icesi.edu.co>
2023-04-20 15:48:16 -0500

    Another advance of authorization for this project

--- src/main/java/com/example/demo/config/SecutiryConfiguration.java
+++ src/main/java/com/example/demo/config/SecutiryConfiguration.java
@@ -1,11 +1,24 @@
 package com.example.demo.config;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.postgresql.shaded.com.ongres.scram.common.exception.ScramException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
 import lombok.AllArgsConstructor;
 
@@ -21,5 +34,24 @@ public class SecutiryConfiguration {
     @Bean
     public AuthenticationManager authenticationManager() {
         return new ProviderManager(new DaoAuthenticationProvider());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthorizationManager<RequestAuthorizationContext> access) throws Exception {
+        return http 
+            .cors(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth 
+                .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        byte[] bytes = secret.getBytes();
+        SecretKeySpec key = new SecretKeySpec(bytes, 0, bytes.length, "RSA");
+        return NimbusJwtDecoder.withSecretKey(key).macAlgorithm(MacAlgorithm.HS512).build();
     }
 }
