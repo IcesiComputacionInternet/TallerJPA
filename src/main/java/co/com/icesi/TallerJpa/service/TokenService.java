1efb7747f4f33d3619c9d5817aa81db432a59e0f
Gilmar Andres Amezquita Romero <1006351546@u.icesi.edu.co>
2023-04-27 15:13:48 -0500

    Added token service

---
+++ src/main/java/co/com/icesi/TallerJpa/service/TokenService.java
@@ -0,0 +1,41 @@
+package co.com.icesi.TallerJpa.service;
+
+import co.com.icesi.TallerJpa.security.CustomAuthentication;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class TokenService {
+    private final JwtEncoder jwtEncoder;
+
+    public String generateToken(Authentication authentication){
+        CustomAuthentication customAuthentication = (CustomAuthentication) authentication;
+        Instant now = Instant.now();
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.HOURS))
+                .subject(authentication.getName())
+                .claim("scope",scope)
+                .claim("icesiUserId",customAuthentication.getUserId())
+                .build();
+        var encoderParameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS512).build(), claims);
+        return this.jwtEncoder.encode(encoderParameters).getTokenValue();
+    }
+}
