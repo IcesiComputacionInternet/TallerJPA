5cb14146c25dd384e32e62ac90147b9d2399c68a
Santiago Arevalo Valencia <santiguitar2003@gmail.com>
2023-05-02 16:00:41 -0500

    refactor: commandLineRunner moved to TestConfigurationData class

--- jpa/src/main/java/com/example/jpa/JpaProjectApplication.java
+++ jpa/src/main/java/com/example/jpa/JpaProjectApplication.java
@@ -19,43 +19,4 @@ public class JpaProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpaProjectApplication.class, args);
 	}
-
-	@Bean
-	CommandLineRunner commandLineRunner(UserRepository users,
-										RoleRepository roleRepository,
-										PasswordEncoder encoder) {
-		IcesiRole icesiRole = IcesiRole.builder()
-				.roleId(UUID.randomUUID())
-				.description("Role for demo")
-				.name("ADMIN")
-				.build();
-		IcesiRole icesiRole2 = IcesiRole.builder()
-				.roleId(UUID.randomUUID())
-				.description("Role for demo")
-				.name("USER")
-				.build();
-		IcesiUser icesiUser = IcesiUser.builder()
-				.userId(UUID.randomUUID())
-				.firstName("John")
-				.lastName("Doe")
-				.password(encoder.encode("password"))
-				.email("johndoe@email.com")
-				.phoneNumber("123456789")
-				.role(icesiRole)
-				.build();
-		IcesiUser icesiUser2 = IcesiUser.builder()
-				.userId(UUID.randomUUID())
-				.firstName("John")
-				.lastName("Doe")
-				.password(encoder.encode("password"))
-				.email("johndoe2@email.com")
-				.phoneNumber("135791113")
-				.role(icesiRole2)
-				.build();
-
-		return args -> {
-			users.save(icesiUser);
-			users.save(icesiUser2);
-		};
-	}
 }
