43d7651fd0eece864f7a9c0a8f88e5160c977ce5
Rockthor <jan.carvajal1@u.icesi.edu.co>
2023-05-21 16:37:20 -0500

    I forgot one exception xd

--- src/main/java/com/example/demo/service/IcesiRoleService.java
+++ src/main/java/com/example/demo/service/IcesiRoleService.java
@@ -3,10 +3,14 @@ package com.example.demo.service;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.DTO.IcesiRoleCreateDTO;
 import com.example.demo.DTO.ResponseIcesiRoleDTO;
+import com.example.demo.error.exception.DetailBuilder;
+import com.example.demo.error.exception.ErrorCode;
+import com.example.demo.error.util.IcesiExceptionBuilder;
 import com.example.demo.mapper.IcesiRoleMapper;
 import com.example.demo.model.IcesiRole;
 import com.example.demo.repository.IcesiRoleRepository;
@@ -24,7 +28,13 @@ public class IcesiRoleService {
 
         Optional<IcesiRole> existingIcesiRole = icesiRoleRepository.findByName(icesiRoleCreateDTO.getName());
 
-        existingIcesiRole.ifPresent(u -> {throw new RuntimeException("This role name is already in use");});
+        existingIcesiRole.ifPresent(u -> {
+            throw IcesiExceptionBuilder.createIcesiException(
+                "This role name is already in use",
+                HttpStatus.BAD_REQUEST,
+                new DetailBuilder(ErrorCode.ERR_400, "role", "name", icesiRoleCreateDTO.getName())
+            ).get();
+        });
 
         IcesiRole newIcesiRole = icesiRoleMapper.fromIcesiRoleCreateDTO(icesiRoleCreateDTO);
         newIcesiRole.setRoleId(UUID.randomUUID());
--- src/main/java/com/example/demo/service/IcesiUserService.java
+++ src/main/java/com/example/demo/service/IcesiUserService.java
@@ -1,9 +1,14 @@
 package com.example.demo.service;
 import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import com.example.demo.DTO.IcesiUserCreateDTO;
 import com.example.demo.DTO.ResponseIcesiUserDTO;
+import com.example.demo.error.exception.DetailBuilder;
+import com.example.demo.error.exception.ErrorCode;
+import com.example.demo.error.util.IcesiExceptionBuilder;
 import com.example.demo.mapper.IcesiUserMapper;
 import com.example.demo.model.IcesiRole;
 import com.example.demo.model.IcesiUser;
@@ -26,14 +31,35 @@ public class IcesiUserService {
         Optional<IcesiUser> userWithPhone = icesiUserRepository.findByPhoneNumber(user.getPhoneNumber());
         
         if (userWithEmail.isPresent() && userWithPhone.isPresent()) {
-            throw new RuntimeException("These email and phone number are already in use");
+            throw IcesiExceptionBuilder.createIcesiException(
+                "These email and phone number are already in use",
+                HttpStatus.BAD_REQUEST,
+                new DetailBuilder(ErrorCode.ERR_400, "email and phone number", user.getEmail() + " " + user.getPhoneNumber())
+            ).get();
         }
 
-        userWithEmail.ifPresent(u -> {throw new RuntimeException("This email is already in use");});
-        userWithPhone.ifPresent(u -> {throw new RuntimeException("This phone number is already in use");});
+        userWithEmail.ifPresent(u -> {
+            throw IcesiExceptionBuilder.createIcesiException(
+                "This email is already in use",
+                HttpStatus.BAD_REQUEST,
+                new DetailBuilder(ErrorCode.ERR_400, "email", user.getEmail())
+            ).get();
+        });
+
+        userWithPhone.ifPresent(u -> {
+            throw IcesiExceptionBuilder.createIcesiException(
+                "This phone number is already in use",
+                HttpStatus.BAD_REQUEST,
+                new DetailBuilder(ErrorCode.ERR_400, "phone number", user.getPhoneNumber())
+            ).get();
+        });
         
         IcesiRole icesiRole = icesiRoleRepository.findByName(user.getIcesiRoleCreateDTO().getName())
-            .orElseThrow(() -> new RuntimeException("This role is not present in the database"));
+            .orElseThrow(() -> IcesiExceptionBuilder.createIcesiException(
+                "This role is not present in the database",
+                HttpStatus.NOT_FOUND,
+                new DetailBuilder(ErrorCode.ERR_404, "role", "name", user.getIcesiRoleCreateDTO().getName())
+            ).get());
         IcesiUser icesiUser = icesiUserMapper.fromIcesiUserCreateDTO(user);
 
         icesiUser.setIcesiRole(icesiRole);
