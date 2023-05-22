baea601c51e2700cf3f48b45571cbcda12d8c19d
DaniBonica001 <1107834340@u.icesi.edu.co>
2023-05-21 20:55:10 -0500

    Config files

---
+++ src/main/java/co/com/icesi/TallerJPA/dto/TokenDTO.java
@@ -0,0 +1,14 @@
+package co.com.icesi.TallerJPA.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDTO {
+    private String token;
+}
