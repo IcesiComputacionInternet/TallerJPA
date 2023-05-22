2e625ebfd3d8b296f02c8cc42dc8057c74873abc
Santiago Arevalo Valencia <santiguitar2003@gmail.com>
2023-05-22 10:16:46 -0500

    refactor: unnecessary import deleted

--- jpa/src/main/java/com/example/jpa/api/RoleAPI.java
+++ jpa/src/main/java/com/example/jpa/api/RoleAPI.java
@@ -2,6 +2,7 @@ package com.example.jpa.api;
 
 import com.example.jpa.dto.RoleDTO;
 import com.example.jpa.model.IcesiRole;
+import com.sun.xml.bind.v2.TODO;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
