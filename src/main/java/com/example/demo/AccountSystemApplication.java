70662d49f9faa8b684ae0d9171f02ecd14d5cd22
BrayanOB2003 <brayanortizbastidas82@gmail.com>
2023-05-22 07:51:13 -0500

    Added account api

--- src/main/java/com/example/demo/AccountSystemApplication.java
+++ src/main/java/com/example/demo/AccountSystemApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AccountSystemApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args
+    ) {
         SpringApplication.run(AccountSystemApplication.class, args);
     }
     
