70d0fa874e4c56052e9e261847537a388df88910
Gilmar Andres Amezquita Romero <1006351546@u.icesi.edu.co>
2023-05-21 22:35:47 -0500

    Corrected mappers

--- src/main/java/co/com/icesi/TallerJpa/mapper/IcesiAccountMapper.java
+++ src/main/java/co/com/icesi/TallerJpa/mapper/IcesiAccountMapper.java
@@ -4,9 +4,16 @@ import co.com.icesi.TallerJpa.dto.IcesiAccountRequestDTO;
 import co.com.icesi.TallerJpa.dto.IcesiAccountResponseDTO;
 import co.com.icesi.TallerJpa.model.IcesiAccount;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IcesiAccountMapper {
+    @Mapping(target = "icesiUser", source = "userEmail", ignore = true)
+    @Mapping(target = "accountId", ignore = true)
+    @Mapping(target = "accountNumber", ignore = true)
+    @Mapping(target = "active", ignore = true)
     IcesiAccount fromAccountDto(IcesiAccountRequestDTO icesiAccountRequestDTO);
+    @Mapping(target = "user", source = "icesiUser")
+    @Mapping(target = "user.role", source = "icesiUser.icesiRole")
     IcesiAccountResponseDTO fromIcesiAccountToResponse(IcesiAccount icesiAccount);
 }
--- src/main/java/co/com/icesi/TallerJpa/mapper/IcesiUserMapper.java
+++ src/main/java/co/com/icesi/TallerJpa/mapper/IcesiUserMapper.java
@@ -4,9 +4,12 @@ import co.com.icesi.TallerJpa.dto.IcesiUserRequestDTO;
 import co.com.icesi.TallerJpa.dto.IcesiUserResponseDTO;
 import co.com.icesi.TallerJpa.model.IcesiUser;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IcesiUserMapper {
+    @Mapping(target = "icesiRole", source = "role", ignore = true)
     IcesiUser fromUserDto(IcesiUserRequestDTO icesiUserRequestDTO);
+    @Mapping(target = "role", source = "icesiRole")
     IcesiUserResponseDTO fromIcesiUserToResponse(IcesiUser icesiUser);
 }
