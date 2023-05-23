13687cdad805cd517a36bb98d2495ad28c192c72
Juan Pablo Ramos Torres <61593415+Juan-dev123@users.noreply.github.com>
2023-05-23 15:27:24 -0500

    add default values to the data base when the app is initialized

--- src/main/java/co/edu/icesi/tallerjpa/TallerJpaApplication.java
+++ src/main/java/co/edu/icesi/tallerjpa/TallerJpaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
@@ -21,60 +22,61 @@ public class TallerJpaApplication {
 		SpringApplication.run(TallerJpaApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner commandLineRunner(IcesiUserRepository users,
-//										IcesiRoleRepository icesiroleRepository,
-//										PasswordEncoder encoder) {
-//		IcesiRole icesiRole = IcesiRole.builder()
-//				.roleId(UUID.fromString("f218a75c-c6af-4f1e-a2c6-b2b47f1a0678"))
-//				.description("Role for demo")
-//				.name(NameIcesiRole.ADMIN.toString())
-//				.build();
-//		IcesiRole icesiRole2 = IcesiRole.builder()
-//				.roleId(UUID.fromString("4fd2fef7-e595-48fe-a368-94edd6e142ee"))
-//				.description("Role for demo")
-//				.name(NameIcesiRole.USER.toString())
-//				.build();
-//		IcesiRole icesiRole3 = IcesiRole.builder()
-//				.roleId(UUID.fromString("3fd2fef7-e595-48fe-a368-94edd6e142ee"))
-//				.description("Role for demo")
-//				.name(NameIcesiRole.BANK.toString())
-//				.build();
-//		IcesiUser icesiUser = IcesiUser.builder()
-//				.userId(UUID.fromString("04dacbf5-4815-4d6e-a2a7-db01a607f237"))
-//				.icesiRole(icesiRole)
-//				.firstName("John")
-//				.lastName("Doe")
-//				.email("johndoe@email.com")
-//				.phoneNumber("+57123123123")
-//				.password(encoder.encode("password"))
-//				.build();
-//		IcesiUser icesiUser2 = IcesiUser.builder()
-//				.userId(UUID.fromString("df17e266-dcc4-4bf2-923c-bb5559722f50"))
-//				.firstName("John")
-//				.lastName("Doe")
-//				.email("johndoe2@email.com")
-//				.icesiRole(icesiRole2)
-//				.phoneNumber("+57123123123")
-//				.password(encoder.encode("password"))
-//				.build();
-//		IcesiUser icesiUser3 = IcesiUser.builder()
-//				.userId(UUID.fromString("af17e266-dcc4-4bf2-923c-bb5559722f50"))
-//				.firstName("John")
-//				.lastName("Doe")
-//				.email("johndoe3@email.com")
-//				.icesiRole(icesiRole3)
-//				.phoneNumber("+57123123123")
-//				.password(encoder.encode("password"))
-//				.build();
-//
-//		return args -> {
-//			icesiroleRepository.save(icesiRole);
-//			icesiroleRepository.save(icesiRole2);
-//			icesiroleRepository.save(icesiRole3);
-//			users.save(icesiUser);
-//			users.save(icesiUser2);
-//			users.save(icesiUser3);
-//		};
-//	}
+	@Bean
+	@Profile("!test")
+	CommandLineRunner commandLineRunner(IcesiUserRepository users,
+										IcesiRoleRepository icesiroleRepository,
+										PasswordEncoder encoder) {
+		IcesiRole icesiRole = IcesiRole.builder()
+				.roleId(UUID.fromString("f218a75c-c6af-4f1e-a2c6-b2b47f1a0678"))
+				.description("Role for demo")
+				.name(NameIcesiRole.ADMIN.toString())
+				.build();
+		IcesiRole icesiRole2 = IcesiRole.builder()
+				.roleId(UUID.fromString("4fd2fef7-e595-48fe-a368-94edd6e142ee"))
+				.description("Role for demo")
+				.name(NameIcesiRole.USER.toString())
+				.build();
+		IcesiRole icesiRole3 = IcesiRole.builder()
+				.roleId(UUID.fromString("3fd2fef7-e595-48fe-a368-94edd6e142ee"))
+				.description("Role for demo")
+				.name(NameIcesiRole.BANK.toString())
+				.build();
+		IcesiUser icesiUser = IcesiUser.builder()
+				.userId(UUID.fromString("04dacbf5-4815-4d6e-a2a7-db01a607f237"))
+				.icesiRole(icesiRole)
+				.firstName("John")
+				.lastName("Doe")
+				.email("johndoe@email.com")
+				.phoneNumber("+57123123123")
+				.password(encoder.encode("password"))
+				.build();
+		IcesiUser icesiUser2 = IcesiUser.builder()
+				.userId(UUID.fromString("df17e266-dcc4-4bf2-923c-bb5559722f50"))
+				.firstName("John")
+				.lastName("Doe")
+				.email("johndoe2@email.com")
+				.icesiRole(icesiRole2)
+				.phoneNumber("+57123123123")
+				.password(encoder.encode("password"))
+				.build();
+		IcesiUser icesiUser3 = IcesiUser.builder()
+				.userId(UUID.fromString("af17e266-dcc4-4bf2-923c-bb5559722f50"))
+				.firstName("John")
+				.lastName("Doe")
+				.email("johndoe3@email.com")
+				.icesiRole(icesiRole3)
+				.phoneNumber("+57123123123")
+				.password(encoder.encode("password"))
+				.build();
+
+		return args -> {
+			icesiroleRepository.save(icesiRole);
+			icesiroleRepository.save(icesiRole2);
+			icesiroleRepository.save(icesiRole3);
+			users.save(icesiUser);
+			users.save(icesiUser2);
+			users.save(icesiUser3);
+		};
+	}
 }
