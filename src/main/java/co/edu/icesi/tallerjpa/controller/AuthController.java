3fc9c87a9cf13c537f6f39ba30b1b2fbef34b05f
Juan Pablo Ramos Torres <61593415+Juan-dev123@users.noreply.github.com>
2023-05-27 20:20:09 -0500

    enable cors for AuthController

--- src/main/java/co/edu/icesi/tallerjpa/controller/AuthController.java
+++ src/main/java/co/edu/icesi/tallerjpa/controller/AuthController.java
@@ -7,12 +7,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins="*")
 public class AuthController {
     private final TokenService tokenService;
     private final AuthenticationManager authenticationManager;
